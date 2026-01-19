b4fe2c6ba30e42022dc94f06126265cd8949ba0e
syedaftab-dev <syed.md.aftab.2027@gmail.com>
2026-01-19 22:23:14 +0530

    add new images and update layout for section 2; enhance canvas background behavior on hover

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,24 @@ function App() {
   return (
     <>
       <main>
-        <Canvas style={{
+        <div className='images'>
+          <img id='tomorrowland' src="/tomorrowland.png" alt="" />
+          <img id='navy-pier' src="/navy-pier.png" alt="" />
+          <img id='msi-chicago' src="/msi-chicago.png" alt="" />
+          <img id='phone' src="/phone.png" alt="" />
+          <img id='kikk' src="/kikk.png" alt="" />
+          <img id='kennedy' src="/kennedy.png" alt="" />
+          <img id='opera' src="/opera.png" alt="" />
+        </div>
+        <Canvas
+        id='canvas-elem'
+        style={{
           height: "100vh",
           width: "100vw",
           position: "fixed",
           top: 0,
           left: 0,
           zIndex: 1,
-          backgroundImage: "url(/background-m.png)",
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover"
         }}>
           <Dog />
         </Canvas>
@@ -69,7 +77,38 @@ function App() {
           <div className='first-line'></div>
           <div className='second-line'></div>
         </section>
-        <section id='section-2'></section>
+        <section id='section-2'>
+          <div className="titles">
+            <div img-title="tomorrowland" className="title">
+              <small>2020 - ONGOING</small>
+              <h1>Tomorrowland</h1>
+            </div>
+            <div img-title="navy-pier" className="title">
+              <small>2018 - ONGOING </small>
+              <h1>Navy Pier</h1>
+            </div>
+            <div img-title="msi-chicago" className="title">
+              <small>2015 - ONGOING</small>
+              <h1>MSI Chicago</h1>
+            </div>
+            <div img-title="phone" className="title">
+              <small>2014 - ONGOING</small>
+              <h1>This Was Louise’s Phone</h1>
+            </div>
+            <div img-title="kikk" className="title">
+              <small>2012 - ONGOING</small>
+              <h1>KIKK Festival 2018</h1>
+            </div>
+            <div img-title="kennedy" className="title">
+              <small>2017 - ONGOING</small>
+              <h1>The Kennedy Center</h1>
+            </div>
+            <div img-title="opera" className="title">
+              <small>2016 - ONGOING</small>
+              <h1>Royal Opera Of Wallonia</h1>
+            </div>
+          </div>
+        </section>
         <section id='section-3'></section>
       </main>
     </>
